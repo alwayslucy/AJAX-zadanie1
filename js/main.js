422b62a10fb4d8d856efba1fea3f4c96c97782a0
alwayslucy <lucyna.gamon@gmail.com>
2021-07-05 17:14:27 +0200

    drugi commit

--- js/main.js
+++ js/main.js
@@ -7,8 +7,17 @@ $(document).ready(function(){
 
     btn.click(function(){
 
-         $.get('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data) {
-            $('#dane-programisty').text(data);
+         $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php', function(data) {
+
+            let imieProgramisty = $('<p></p>').text(`Imie: ${data.imie}`);
+            let nazwiskoProgramisty = $('<p></p>').text(`Nazwisko: ${data.nazwisko}`);
+            let zawodProgramisty = $('<p></p>').text(`Zawod: ${data.zawod}`);
+            let firmaProgramisty = $('<p></p>').text(`Firma: ${data.firma}`);
+
+            $(newDiv).append(imieProgramisty);
+            $(newDiv).append(nazwiskoProgramisty);
+            $(newDiv).append(zawodProgramisty);
+            $(newDiv).append(firmaProgramisty);
         });
     });
     
